314c6f20e66b81b621aa9faa63313aa36dc85b03
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-06-29 11:23:57 -0700

    🔥 Delete unused argument

--- feral_cat_eradication/feral_cat_eradication.py
+++ feral_cat_eradication/feral_cat_eradication.py
@@ -6,7 +6,7 @@ def calculate_max_lambda(leslie_matrix):
 
 
 def calculate_stable_stage(leslie_matrix):
-    eigenvalues, eigenvectors = np.linalg.eig(leslie_matrix)
+    _, eigenvectors = np.linalg.eig(leslie_matrix)
     proyection = np.matmul(leslie_matrix, eigenvectors.real)
     return proyection / np.sum(proyection)
 
@@ -22,7 +22,7 @@ def calculate_reproductive_value(leslie_matrix, max_age=7):
     return calculate_max_lambda(reproductive_matrix)
 
 
-def calculate_generation_function(leslie_matrix, max_age=7):
+def calculate_generation_function(leslie_matrix):
     reproductive_values = np.log(calculate_reproductive_value(leslie_matrix))
     max_lambda_log = np.log(calculate_max_lambda(leslie_matrix))
     return reproductive_values / max_lambda_log
